f43417f32a076ba7e06b0cd9cdd7dcf47e3e9976
Nguyen Duc <DucNM.B21CN249@stu.ptit.edu.vn>
2025-10-13 22:51:43 +0700

    Config firebase

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,14 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'core/theme/theme_service.dart';
 import 'modules/app.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   Bloc.observer = AppBlocObserver();
   await EasyLocalization.ensureInitialized();
   await ThemeService.init();
